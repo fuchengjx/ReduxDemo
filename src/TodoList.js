dba75f3d28ec7d9066e6f9b1c2850e64c972ab18
fc <327619852@qq.com>
2019-07-02 12:11:24 +0800

    add todoAdd

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,19 +1,62 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux'  //引用的只是react-redux里的部分，所以要用解构赋值
 class TodoList extends Component {
+  constructor(props) {
+    super(props)
+    // this.state = store.getState()
+  }
   render() {
     return (
       <div>
         <div>
-          <input></input>
-          <button>提交</button>
+          <input value={this.props.inputValue} onChange={ this.props.changeInputValue }></input>
+          <button onClick={this.props.handleBtnClick.bind(this)}>提交</button>
         </div>
         <ul>
-          
+          {
+            this.props.list.map((item, index) => {
+              return <li key={index}>{item}</li>
+            })
+          }
         </ul>
       </div>
     )
   }
+
+ 
+
 }
 
-export default TodoList;
+
+
+//把store里的数据映射到组件的props里  
+const mapStateToProps = (state) => {  //state参数里的数据就是store内的数据
+  return {
+    inputValue: state.inputValue,
+    list: state.list
+  }
+}
+
+// store.dispatch, props 把store.dispatch方法映射到props里  通过props调用dispatch里的方法改变store的值
+const mapDispatchToProps = (dispatch) => {
+  return {
+    changeInputValue(e) {
+      const action = {
+        type: 'change_input_value',
+        value: e.target.value
+      }
+      dispatch(action)
+    },
+
+    handleBtnClick() {
+      console.log(this.props.inputValue)
+      const action = {
+        type: 'add_todo_item',
+        // value: this.props.inputValue
+      }
+      dispatch(action)
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);  // connect方法与Provider做连接  获取store里的数据
--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux'
+import store from './store'
 
 import TodoList from './TodoList'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<TodoList />, document.getElementById('root'));
+
+//自定义App组件  Provider组件将store全部提供给被它包裹的组件。 Provider内部的组件都可以获得store里的数据。
+const App = (
+  <Provider store={store}>
+    <TodoList/>
+  </Provider>
+)
+
+
+ReactDOM.render(App, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
