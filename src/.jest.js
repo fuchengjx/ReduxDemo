ed2aabd8d2411ce587d292f256b6be98a1aff933
傅诚 <327619852@qq.com>
2019-07-16 15:27:21 +0800

    feat: jest reducer

---
+++ src/.jest.js
@@ -0,0 +1,29 @@
+module.exports = {
+  setupFiles: [
+    './test/setup.js',
+  ],
+  moduleFileExtensions: [
+    'js',
+    'jsx',
+  ],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+  ],
+  testRegex: '.*\\.test\\.js$',
+  collectCoverage: false,
+  collectCoverageFrom: [
+    'src/components/**/*.{js}',
+  ],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|less|scss)$": "<rootDir>/__mocks__/styleMock.js"
+  },
+  transform: {
+    "^.+\\.js$": "babel-jest"
+  },
+};
+
+作者：supot
+链接：https://juejin.im/post/5b6c39bde51d45195c079d62
+来源：掘金
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
